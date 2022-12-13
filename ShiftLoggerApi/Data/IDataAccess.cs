46a2ab6d74153c76c92d5857e2f5e938809aa809
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-13 08:15:22 +0300

    Change method signatures to Async

--- ShiftLoggerApi/Data/IDataAccess.cs
+++ ShiftLoggerApi/Data/IDataAccess.cs
@@ -4,9 +4,9 @@ namespace ShiftLoggerApi.Data;
 
 public interface IDataAccess
 {
-    public List<Shift> GetShifts();
-    public Shift GetShiftById(int id);
-    public void AddShift(Shift shift);
-    public void UpdateShift(int id, Shift shift);
-    public void DeleteShift(int id);
+    public Task<List<Shift>> GetShiftsAsync();
+    public Task<Shift> GetShiftByIdAsync(int id);
+    public Task AddShiftAsync(Shift shift);
+    public Task UpdateShiftAsync(int id, Shift shift);
+    public Task DeleteShiftAsync(int id);
 }
