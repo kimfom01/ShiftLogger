faf8d1cb47a249427d60309aaf35ae222b0d09eb
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-13 07:24:34 +0300

    Update GetShift method to take an id

--- ShiftLoggerApi/Data/IDataAccess.cs
+++ ShiftLoggerApi/Data/IDataAccess.cs
@@ -5,7 +5,7 @@ namespace ShiftLoggerApi.Data;
 public interface IDataAccess
 {
     public List<Shift> GetShifts();
-    public Shift GetShift();
+    public Shift GetShift(int id);
     public void AddShift(Shift shift);
     public void UpdateShift(int id, Shift shift);
     public void DeleteShift(int id);
