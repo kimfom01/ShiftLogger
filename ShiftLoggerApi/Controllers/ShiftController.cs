aa5085429f374e3fda972ad947c484d208e0394a
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-13 07:25:41 +0300

    Implement the controller methods

--- ShiftLoggerApi/Controllers/ShiftController.cs
+++ ShiftLoggerApi/Controllers/ShiftController.cs
@@ -4,43 +4,55 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ShiftLoggerApi.Data;
+using ShiftLoggerApi.Models;
 
 namespace ShiftLoggerApi.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class ShiftController : ControllerBase
     {
+        private readonly IDataAccess _dataAccess;
+
+        public ShiftController(IDataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+        
         // GET: api/Shift
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Shift> GetShifts()
         {
-            return new string[] { "value1", "value2" };
+            return _dataAccess.GetShifts();
         }
 
         // GET: api/Shift/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public Shift GetShift(int id)
         {
-            return "value";
+            return _dataAccess.GetShift(id);
         }
 
         // POST: api/Shift
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] Shift shift)
         {
+            _dataAccess.AddShift(shift);
         }
 
         // PUT: api/Shift/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody] Shift shift)
         {
+            _dataAccess.UpdateShift(id, shift);
         }
 
         // DELETE: api/Shift/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            _dataAccess.DeleteShift(id);
         }
     }
 }
