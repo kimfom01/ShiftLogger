edd58e80377700401f0e1ce0ee1b0e9f7d5317a8
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-13 07:17:58 +0300

    Implement data access interface using entity framework

---
+++ ShiftLoggerApi/Data/EntityFrameworkDataAccess.cs
@@ -0,0 +1,39 @@
+using ShiftLoggerApi.DataContext;
+using ShiftLoggerApi.Models;
+
+namespace ShiftLoggerApi.Data;
+
+public class EntityFrameworkDataAccess : IDataAccess
+{
+    private readonly ShiftContext _shiftContext;
+
+    public EntityFrameworkDataAccess(ShiftContext shiftContext)
+    {
+        _shiftContext = shiftContext;
+    }
+    
+    public List<Shift> GetShifts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Shift GetShift()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddShift(Shift shift)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateShift(int id, Shift shift)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteShift(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
