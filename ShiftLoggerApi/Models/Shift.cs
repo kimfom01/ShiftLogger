073f7cddf5e9edf329b587c536badf7b29996587
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-13 08:39:56 +0300

    Remove the required attribute on shift model to allow user send lean models to api

--- ShiftLoggerApi/Models/Shift.cs
+++ ShiftLoggerApi/Models/Shift.cs
@@ -6,11 +6,8 @@ public class Shift
 {
     [Key]
     public int Id { get; set; }
-    [Required]
     public string? Name { get; set; }
-    [Required]
     public DateTime StartTime { get; set; }
-
     public DateTime? EndTime { get; set; } = null;
     public string? Duration { get; set; } = null;
 }
