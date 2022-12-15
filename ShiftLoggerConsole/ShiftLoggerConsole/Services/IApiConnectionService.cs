61ccfe87667afea907d230a63259b40072cf8d80
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-15 10:21:17 +0300

    Add new methods to interface

--- ShiftLoggerConsole/ShiftLoggerConsole/Services/IApiConnectionService.cs
+++ ShiftLoggerConsole/ShiftLoggerConsole/Services/IApiConnectionService.cs
@@ -5,4 +5,8 @@ namespace ShiftLoggerConsole.Services;
 public interface IApiConnectionService
 {
     public Task<List<Shift>> GetAllShifts();
+    public Task<Shift> GetShiftById(int id);
+    public Task AddShift(Shift shift);
+    public Task UpdateShift(int id, Shift shift);
+    public Task DeleteShift(int id);
 }
