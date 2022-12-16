71f48a4b52410a3591a8474691bf11a2905fb260
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-16 10:26:51 +0300

    Call the DisplayTable method and remove the foreach loops

--- ShiftLoggerConsole/ShiftLoggerConsole/Controller/ShiftController.cs
+++ ShiftLoggerConsole/ShiftLoggerConsole/Controller/ShiftController.cs
@@ -1,5 +1,6 @@
 using ShiftLoggerConsole.Models;
 using ShiftLoggerConsole.Services;
+using ShiftLoggerConsole.TableVisualization;
 using ShiftLoggerConsole.UI;
 using ShiftLoggerConsole.UserInput;
 
@@ -10,12 +11,14 @@ public class ShiftController : IShiftController
     private readonly IApiConnectionService _apiConnectionService;
     private readonly Menus _menus;
     private readonly IInput _input;
+    private readonly ITableBuilder _builder;
 
-    public ShiftController(IApiConnectionService apiConnectionService, Menus menus, IInput input)
+    public ShiftController(IApiConnectionService apiConnectionService, Menus menus, IInput input, ITableBuilder builder)
     {
         _apiConnectionService = apiConnectionService;
         _menus = menus;
         _input = input;
+        _builder = builder;
     }
 
     public async Task Start()
@@ -52,26 +55,13 @@ public class ShiftController : IShiftController
             choice = _input.GetInput();
         }
     }
-    
-    private void Continue()
-    {
-        Console.WriteLine("Press any Enter to continue");
-        Console.ReadLine();
-    }
 
     private async Task DisplayAllShifts()
     {
         var shifts = await _apiConnectionService.GetAllShifts();
-
-        foreach (var shift in shifts)
-        {
-            Console.WriteLine(shift.Id);
-            Console.WriteLine(shift.Name);
-            Console.WriteLine(shift.StartTime);
-            Console.WriteLine(shift.EndTime);
-            Console.WriteLine(shift.Duration);
-        }
-
+        
+        _builder.DisplayTable(shifts);
+        
         Continue();
     }
 
@@ -80,15 +70,10 @@ public class ShiftController : IShiftController
         Console.WriteLine("You need to enter an id to get the shift");
         var id = _input.GetId();
         var shift = await _apiConnectionService.GetShiftById(id);
-        
-        // TODO: verify that shift is not null otherwise display "not found!"
 
-        Console.WriteLine(shift.Id);
-        Console.WriteLine(shift.Name);
-        Console.WriteLine(shift.StartTime);
-        Console.WriteLine(shift.EndTime);
-        Console.WriteLine(shift.Duration);
-        
+        var shifts = new List<Shift> { shift };
+        _builder.DisplayTable(shifts);
+
         Continue();
     }
 
@@ -104,7 +89,9 @@ public class ShiftController : IShiftController
 
     private async Task EndShift()
     {
-        // display all shifts so user can know the id to update
+        var shifts = await _apiConnectionService.GetAllShifts();
+        _builder.DisplayTable(shifts);
+        
         Console.WriteLine("To end a shift, we need an id");
         var id = _input.GetId();
         var shift = await _apiConnectionService.GetShiftById(id);
@@ -117,6 +104,9 @@ public class ShiftController : IShiftController
 
     private async Task DeleteShift()
     {
+        var shifts = await _apiConnectionService.GetAllShifts();
+        _builder.DisplayTable(shifts);
+        
         Console.WriteLine("To delete a shift, we need an id");
         var id = _input.GetId();
         await _apiConnectionService.DeleteShift(id);
@@ -129,5 +119,11 @@ public class ShiftController : IShiftController
         var timeSpan = endTime - startTime;
         var duration = timeSpan.Hours;
         return duration.ToString();
+    }
+    
+    private void Continue()
+    {
+        Console.WriteLine("Press any Enter to continue");
+        Console.ReadLine();
     }
 }
