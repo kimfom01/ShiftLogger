b517a4ed343c69731eef549b0c469de3f4fc26fd
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-16 09:22:37 +0300

    Implement interface validation methods

--- ShiftLoggerConsole/ShiftLoggerConsole/Validation/InputValidator.cs
+++ ShiftLoggerConsole/ShiftLoggerConsole/Validation/InputValidator.cs
@@ -2,5 +2,13 @@ namespace ShiftLoggerConsole.Validation;
 
 public class InputValidator : IInputValidator
 {
-    
+    public bool IsValidName(string name)
+    {
+        return !string.IsNullOrEmpty(name);
+    }
+
+    public bool IsValidId(string temp, ref int id)
+    {
+        return int.TryParse(temp, out id);
+    }
 }
