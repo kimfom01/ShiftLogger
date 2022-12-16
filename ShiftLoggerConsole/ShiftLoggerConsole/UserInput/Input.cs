d86d74cacbf6bb8ceba15c1ef3edf092d07dfaea
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2022-12-16 09:23:23 +0300

    Implement the GetId and GetName methods with validation

--- ShiftLoggerConsole/ShiftLoggerConsole/UserInput/Input.cs
+++ ShiftLoggerConsole/ShiftLoggerConsole/UserInput/Input.cs
@@ -1,14 +1,41 @@
+using ShiftLoggerConsole.Validation;
+
 namespace ShiftLoggerConsole.UserInput;
 
 public class Input : IInput
 {
+    private readonly IInputValidator _validator;
+
+    public Input(IInputValidator validator)
+    {
+        _validator = validator;
+    }
     public string GetName()
     {
-        throw new NotImplementedException();
+        Console.Write("Enter name: ");
+        var name = Console.ReadLine().Trim();
+        while (!_validator.IsValidName(name))
+        {
+            Console.WriteLine("Error!");
+            Console.Write("Enter name: ");
+            name = Console.ReadLine().Trim();
+        }
+
+        return name;
     }
 
-    int IInput.GetId()
+    public int GetId()
     {
-        throw new NotImplementedException();
+        int id = -1;
+        Console.Write("Enter id: ");
+        var temp = Console.ReadLine()!.Trim();
+        while (!_validator.IsValidId(temp, ref id))
+        {
+            Console.WriteLine("Error!");
+            Console.Write("Enter id: ");
+            temp = Console.ReadLine()!.Trim();
+        }
+
+        return id;
     }
 }
