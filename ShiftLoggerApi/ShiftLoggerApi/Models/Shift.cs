1c7cacae61f18312fc630dd8ee21e2f762a1db95
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-01-02 01:21:27 +0300

    Remove nullability from Name property of Shift model

--- ShiftLoggerApi/ShiftLoggerApi/Models/Shift.cs
+++ ShiftLoggerApi/ShiftLoggerApi/Models/Shift.cs
@@ -6,7 +6,7 @@ public class Shift
 {
     [Key]
     public int Id { get; set; }
-    public string? Name { get; set; }
+    public string Name { get; set; }
     public DateTime StartTime { get; set; }
     public DateTime? EndTime { get; set; } = null;
     public string? Duration { get; set; } = null;
