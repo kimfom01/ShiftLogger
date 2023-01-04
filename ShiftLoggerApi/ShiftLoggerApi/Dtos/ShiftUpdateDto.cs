6f8f3f673e2f80fdfe000302a5ee79c30478b3a1
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-01-04 16:59:19 +0300

    Remove redundant id property

--- ShiftLoggerApi/ShiftLoggerApi/Dtos/ShiftUpdateDto.cs
+++ ShiftLoggerApi/ShiftLoggerApi/Dtos/ShiftUpdateDto.cs
@@ -2,7 +2,6 @@ namespace ShiftLoggerApi.Dtos;
 
 public class ShiftUpdateDto
 {
-    public int Id { get; set; }
     public DateTime? EndTime { get; set; }
     public string? Duration { get; set; }
 }
